3942cf3bf4dbb628ef9198b05f2d67d0722fd78e
Songu3020 <petersongu2000@gmail.com>
2026-03-23 15:43:57 +0100

    GateSix

---
+++ Java/GateSix/PhaseGateTaskSix.java
@@ -0,0 +1,58 @@
+import java.security.SecureRandon;
+
+import java.util.Scanner;
+
+public class PhaseGateTaskSix {
+
+    public static void main(String[]args){
+
+    Scanner input = new Scanner(System.in);
+
+    SecureRandon randon = new SecureRandon();
+
+    int value = 0;
+
+    int[] numbers = {10,20,30,21,22,33,43,100,70};
+
+    int[] firstNumbers = {4,5,7,8,9,1,6,2,3,6};
+
+    for(int num1:numbers){
+    
+    for(int num:firstNumbers){
+ 
+    int attemp = numbers + firstNumbers; 
+    }
+}
+   
+for(int count = 10; count == 10; count++){
+
+    for(int index = 1; index <= 10; index++){
+
+    System.out.print("Attempt this question:");
+
+    int randonAttempt = 1; + randon.nextInt();
+
+                value++;    
+
+        if(value > 2){  
+              
+    System.out.print("You have two chances letf:");
+        }
+                }
+            }
+      }        
+   
+}
+//import randon()
+//import Scanner
+//use loop
+//collect an ArrayOfNumbers
+//collect input
+//Ask user for Attempts
+//get total Scores Of collected Attempts
+//import java.security.SecureRandon;
+    
+
+    
+
+
---
+++ Python/GateFive/PhaseGateTaskSix.java
@@ -0,0 +1,21 @@
+import java.security.secureRandon;
+import java.util.Scanner;
+public class PhaseGateTaskSix {
+    public static void main(String[]args){
+    Scanner input = new Scanner(System.in);
+    SecureRandon randon = new SecureRandon;
+for(int count =10; count >=10; count++){
+    for(int index = 0; index <= 10; index++){
+    System.out.print("Enter any subtraction of your choice:");
+
+    
+            }
+
+
+
+            }
+
+
+        }
+
+}
